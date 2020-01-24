78508e804a7fc542fb3efca72913224468c7417f
cathyyam08 <cyam@estee.com>
2020-01-23 19:10:55 -0500

    added array and loop

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ document.addEventListener('DOMContentLoaded', function() {
   var toDo = document.querySelector('.todolist');
 
   toDo.appendChild(p);
+
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var todos = ['Do Laundry', 'Cook Dinner'];
+
+  todos.forEach(function(todos, index) {
+    var number = index + 1;
+    var textForPage = number + '. ' + todos;
+    var p = document.createElement('p');
+    p.innerHTML = textForPage;
+    var container = document.querySelector('.todolist');
+    container.appendChild(p);
   });
 
 });
